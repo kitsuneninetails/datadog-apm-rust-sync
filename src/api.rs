16235cccb96e0662dce2a86e60ec7255aa32c2e5
Michael Micucci <9975355+kitsuneninetails@users.noreply.github.com>
2020-05-26 15:28:29 +0900

    cargofmt

--- src/api.rs
+++ src/api.rs
@@ -1,9 +1,9 @@
 use crate::model::Span;
+use serde::Serialize;
 use std::{
     collections::HashMap,
     time::{Duration, UNIX_EPOCH},
 };
-use serde::Serialize;
 
 fn fill_meta(span: &Span, env: Option<String>) -> HashMap<String, String> {
     let mut meta = HashMap::new();
