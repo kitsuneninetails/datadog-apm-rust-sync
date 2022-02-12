e0d4ef747f2eafa5cb59ee9a11280651b3539374
Michael Micucci <9975355+kitsuneninetails@users.noreply.github.com>
2022-02-12 16:54:15 +0900

    Increase http client send performance
    Use crossbeam to defer the sending of the spans to threads running in
    the background, rather than blocking the handler thread.  The handler
    thread must remain a single-threaded, serial loop to guarantee the
    ordering of all the different spans, otherwise they could get
    out-of-order.  This means that any hiccup in this thread will slow down
    the processing, so that the inflow of new requests could back up.  If
    this is the normal case (where inflow > outflow), the channel will
    necessarily back up, causing more and more memory to be used.
    
    To help unblock the handler channel so that the outflow can be
    maximized, any I/O should be offloaded to other threads.  This has the
    advantage of allowing more than one thread to handle certain operations,
    since the ordering has already been set.  In the case of the HTTP client
    send, the spans have all been collated and the trace has been closed,
    meaning the order is now moot and the various event sends can be handled
    in parallel with no fear of misordering.  This means the time-intensive
    HTTP call can be farmed out to a number of helper threads, whereas
    before, the main thread was doing the work each time in a blocking
    fashion.
    
    This has seen a remarkable performance increase, although there still
    remains issues when using the standard logger, as the span/trace ID
    lookups (to put into the logging message) are too slow.  However, the
    I/O to the datadog agent via HTTP should now no longer be a blocker for
    handling events, thus increasing the throughput of the channels
    immensely.
    
    This was accomplished by spawning a number of helper threads in
    DdAgentClient along with cloned multi-sender, multi-consumer channels
    (courtesy of the crossbeam-channel crate), which handle high-speed
    locking and synchronization between the various receivers.  The "send()"
    command (run when the "send_trace" tag is used in an event! command) now
    only pushes the Span list onto this mpmc channel, and continues on.  The
    threads that start up when the DdAgentClient is created handle the
    actual reading of this shared channel and processing of the client calls
    via HTTP.
    
    The number of these helper threads defaults to 4, but can be set in the
    Config of the DatadogTracing::new() call.

--- tests/leak.rs
+++ tests/leak.rs
@@ -1,5 +1,5 @@
 use datadog_apm_sync::{Config, DatadogTracing, LoggingConfig};
-use log::debug;
+use log::warn;
 use tracing::{event, span};
 
 #[tokio::test(flavor = "multi_thread")]
@@ -19,20 +19,48 @@ async fn test_leak() {
     let _client = DatadogTracing::new(config);
 
     let f1 = tokio::spawn(async {
-        for i in 1..100000000 {
-            leak(i).await.unwrap();
-            leak(i).await.unwrap();
+        for i in 1..100_000_000 {
+            let rate_limit = false; //(i % 100_000) > 99_900;
+            leak(i, rate_limit).await.unwrap();
+            leak(i, rate_limit).await.unwrap();
             event!(tracing::Level::INFO, send_trace = i);
-            if i % 10000 == 0 {
-                std::thread::sleep(std::time::Duration::from_secs(5));
-            }
         }
     });
     f1.await.unwrap();
 }
 
-async fn leak(trace_id: u64) -> Result<(), ()> {
+async fn leak(trace_id: u64, rate_limit: bool) -> Result<(), ()> {
     let _span = span!(tracing::Level::INFO, "leak_test", trace_id = trace_id);
-    debug!("Span");
+    warn!("Span1");
+    warn!("Span2");
+    warn!("Span3");
+    warn!("Span4");
+    leak_inner1(trace_id).await;
+    leak_inner2(trace_id).await;
+    if rate_limit {
+        std::thread::sleep(std::time::Duration::from_secs(1));
+    }
     Ok(())
+}
+
+async fn leak_inner1(trace_id: u64) {
+    let _span = span!(tracing::Level::INFO, "leak_test_inner", trace_id = trace_id);
+    warn!("Span_inner1");
+    warn!("Span_inner2");
+    warn!("Span_inner3");
+    leak_inner_inner(trace_id).await;
+}
+
+async fn leak_inner2(trace_id: u64) {
+    let _span = span!(tracing::Level::INFO, "leak_test_inner", trace_id = trace_id);
+    warn!("Span_inner1b");
+    warn!("Span_inner2b");
+    warn!("Span_inner3b");
+}
+
+async fn leak_inner_inner(trace_id: u64) {
+    let _span = span!(tracing::Level::INFO, "leak_test_inner", trace_id = trace_id);
+    warn!("Span_inner_inner1");
+    warn!("Span_inner_inner2");
+    warn!("Span_inner_inner3");
 }
